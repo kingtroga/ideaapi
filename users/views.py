2242b0dd0d958c339416f0dc1e1194fe8f4c975f
kingtroga <trogaclassicman@gmail.com>
2023-04-29 21:54:04 +0100

    user login is finally working properly

--- users/views.py
+++ users/views.py
@@ -1,36 +1,23 @@
-from rest_framework import generics
 from dj_rest_auth.views import LoginView
-from .serializers import CustomLoginSerializer, CustomLogoutSerializer
-from rest_framework import status
+from .serializers import CustomLoginSerializer
 from rest_framework.response import Response
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from accounts.models import CustomUser
-from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
 
 
 
 class CustomLoginView(LoginView):
     serializer_class = CustomLoginSerializer
 
-class CustomLogoutView(generics.GenericAPIView):
-    serializer_class = CustomLogoutSerializer
+class CustomLogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user_id = serializer.validated_data['user_id']
-        password = serializer.validated_data['password']
-        try:
-            user = authenticate(request, user_id=user_id, password=password)
-            if user is not None:
-                # delete token from database
-                Token.objects.get(user=user).delete()
-                # return success response
-                return Response({'success': 'Successfully logged out.'})
-            else:
-                # return error response
-                return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
-        except CustomUser.DoesNotExist:
-            # return error response
-            return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        # delete token from database
+        Token.objects.get(user=request.user).delete()
+        # return success response
+        return Response({'success': 'Successfully logged out.'})
