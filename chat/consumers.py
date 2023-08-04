a628026423c3502648059066850bcdc7bd1f4503
kingtroga <trogaclassicman@gmail.com>
2023-08-04 13:37:21 +0100

    Authentication works now

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,8 +5,16 @@ from rest_framework.authtoken.models import Token
 from urllib.parse import parse_qs
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import Room, Message
 
 class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.room_name = None # ??
+        self.room_group_name = None # Group chats
+        self.room = None # All chats names
+        self.user = None # Authenticated user
+
     def returnUser(self, token_string):
         try:
             user = Token.objects.get(key=token_string).user
@@ -16,10 +24,9 @@ class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        self.room_group_name = f'chat_{self.room_name}'
+        self.room, self.created = Room.objects.get_or_create(name=self.room_name)
+        
 
         self.access_token = self.scope["url_route"]["kwargs"]["access_token"]
         print("self.access_token: ", self.access_token)
@@ -30,23 +37,39 @@ class ChatConsumer(WebsocketConsumer):
 
         self.accept()
 
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, 
+            self.channel_name,
+        )
+
 
     def disconnect(self, code):
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
+            self.room_group_name, 
+            self.channel_name,
         )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        if not self.user.is_authenticated:
+            return 
+        
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name, 
+            {
+                "type": "chat_message",
+                'user': self.user.fullName,
+                "message": message,
+            }
+        )
+        Message.objects.create(
+            user=self.user,
+            room=self.room,
+            content=message
         )
 
     def chat_message(self, event):
-        message = event["message"]
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps(event))
