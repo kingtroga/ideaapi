701bdf3721adfaa56839b668c706e57d768948dc
kingtroga <trogaclassicman@gmail.com>
2023-10-01 21:19:15 +0100

    new setting

--- ideaapi/settings.py
+++ ideaapi/settings.py
@@ -49,7 +49,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
